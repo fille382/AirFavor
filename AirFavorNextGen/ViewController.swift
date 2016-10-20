c650c955050b7c5391c04bcdb34c9ce4782ac5be
Filip Bellander <fille382@gmail.com>
2016-10-20 21:12:47 +0200

    Initial Commit

---
+++ AirFavorNextGen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirFavorNextGen
+//
+//  Created by Filip Bellander on 20/10/16.
+//  Copyright © 2016 Filip Bellander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
